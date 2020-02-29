05df8fcded91ba6e43f345a8ceb7758175922dfd
hlolli <hlolli@gmail.com>
2020-02-29 23:35:25 +0100

    polyfill strings (String.prototype.startsWith) for IE11 support

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import * as serviceWorker from "./serviceWorker";
 // POLYFILLS
 import "core-js/es/object";
 import "core-js/es/weak-set";
+import "core-js/es/string";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
