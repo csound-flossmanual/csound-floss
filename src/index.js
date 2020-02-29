aaf868ade33dc88bc284350f564f10642b45469d
hlolli <hlolli@gmail.com>
2020-02-29 23:19:46 +0100

    polyfill for IE11 support

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+// POLYFILLS
+import "core-js/es/object";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
