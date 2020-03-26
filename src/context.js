1b3f03bd369b12f384fcca3926674617b6ea3d6d
hlolli <hlolli@gmail.com>
2020-03-26 22:47:17 +0100

    bumping dependencies and csound-wasm with c++ opcodes support

--- src/context.js
+++ src/context.js
@@ -89,7 +89,7 @@ export const CsoundProvider = ({ children }) => {
     isPaused: false,
     isPlaying: false,
     loadedSamples: [],
-    logDialogOpen: true,
+    logDialogOpen: process.env.NODE_ENV === "production" ? false : true,
     logDialogClosed: false,
     logs: [],
   });
