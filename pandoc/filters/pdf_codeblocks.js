d46dfb54677dcb921e55480b8bb6acb05ca27782
hlolli <hlolli@gmail.com>
2020-06-01 00:24:47 +0200

    fixing header font in pdf and fallback pre block style in pdf

--- pandoc/filters/pdf_codeblocks.js
+++ pandoc/filters/pdf_codeblocks.js
@@ -7,15 +7,11 @@ const { buildLink } = require("../utils");
 const isAbsoluteUrl = url => /^https?:\/\//i.test(url);
 
 function action({ t: type, c: value }, format, meta) {
-  // if (type === "Header") {
-  //   type = "Chapter";
-  //   console.error(type, value);
-  // }
   if (type === "CodeBlock") {
+    if (value[0][1].length === 0) {
+      value[0][1] = ["default"];
+    }
     if (R.last(value).match(/\n\s*\n/g)) {
-      // console.error(
-      //   "Replacing multiple empty lines in example for 1 empty line"
-      // );
       value[R.dec(R.length(value))] = R.last(value).replace(/\n\s*\n/g, "\n\n");
     }
   }
