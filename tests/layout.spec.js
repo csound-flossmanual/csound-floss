8f10e7156a0752653abe3c133d88aa92084fca0f
hlolli <hlolli@gmail.com>
2020-06-03 22:32:12 +0200

    print more verbose error messages when code-width test fails

--- tests/layout.spec.js
+++ tests/layout.spec.js
@@ -7,18 +7,22 @@ const fg = require("fast-glob");
 const allChapters = fg.sync([`${BOOK_DIRECTORY}/*.md`], { dot: false });
 const { execSync } = require("child_process");
 
+const toBeShorterThanFn = (received, limit) => {
+  if (received.length < limit) {
+    return { pass: true };
+  }
+  const helpfulMessage =
+    chalk.rgb(23, 255, 24).inverse(received.substring(0, 78)) +
+    chalk.rgb(230, 10, 24).inverse(received.substring(78));
+  return {
+    message: () => `Too long line: \n\t${helpfulMessage}`,
+    pass: false,
+  };
+};
+
 expect.extend({
   toBeShorterThan(received, limit) {
-    if (received.length < limit) {
-      return { pass: true };
-    }
-    const helpfulMessage =
-      chalk.rgb(23, 255, 24).inverse(received.substring(0, 78)) +
-      chalk.rgb(230, 10, 24).inverse(received.substring(78));
-    return {
-      message: () => `Too long line: \n\t${helpfulMessage}`,
-      pass: false,
-    };
+    return toBeShorterThanFn(received, limit);
   },
 });
 
@@ -38,6 +42,15 @@ each(allChapters).describe(`Testing %s`, mdPath => {
       R.pluck("c"),
       R.map(R.last)
     )(ast);
+
+    // Print all possible failures pre-emptively
+    codeBlocks.forEach(txt => {
+      const lines = txt.split(/\r?\n/);
+      lines.forEach(line => {
+        const { pass, message } = toBeShorterThanFn(line, 79);
+        !pass && console.error(mdPath, message());
+      });
+    });
 
     codeBlocks.forEach(txt => {
       const lines = txt.split(/\r?\n/);
