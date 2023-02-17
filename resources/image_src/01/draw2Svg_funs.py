7595fe98f2904a1243539c19ce4775c21f713582
jh <jh@joachimheintz.de>
2023-02-17 16:57:11 +0100

    tutorial 07 with figures

--- resources/image_src/01/draw2Svg_funs.py
+++ resources/image_src/01/draw2Svg_funs.py
@@ -117,3 +117,13 @@ def scale(inval,inmin,inmax,outmin,outmax):
     """scales inval which is between inmin and inmax
     to the range between outmin and outmax"""
     return (inval-inmin)*((outmax-outmin)/(inmax-inmin))+outmin
+
+def text(txt='',x=10,y=10,fontsize=14,**args):
+    """wrapper for d.append(dw.Text(...))
+    with fontsize set to 14 as default"""
+    d.append(dw.Text(txt,fontsize,x,y,**args))
+    
+def line(x1=10,y1=10,x2=100,y2=100,c='black',**args):
+    """wrapper for d.append(dw.Line(...))
+    with c (color) = black as default"""
+    d.append(dw.Line(x1,y1,x2,y2,stroke=c,**args))
